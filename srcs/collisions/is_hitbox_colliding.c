cc1772c21e84e5b328d074b1438519bdfef07568
Alienxbe <martin.ykman@gmail.com>
2024-04-07 17:23:42 +0200

    Better collision precision with map

--- srcs/collisions/is_hitbox_colliding.c
+++ srcs/collisions/is_hitbox_colliding.c
@@ -6,7 +6,7 @@
 /*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 14:51:09 by marykman          #+#    #+#             */
-/*   Updated: 2024/03/05 13:38:34 by marykman         ###   ########.fr       */
+/*   Updated: 2024/04/07 17:21:47 by marykman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	is_hitbox_colliding(const t_map *map, t_ftype f, t_fpoint pos,
 	int			i;
 	int			collision;
 
-	points[0] = (t_fpoint){pos.x + hitbox.p1.x,
-		pos.y + hitbox.p1.y};
-	points[1] = (t_fpoint){pos.x + hitbox.p2.x,
-		pos.y + hitbox.p1.y};
-	points[2] = (t_fpoint){pos.x + hitbox.p2.x,
-		pos.y + hitbox.p2.y};
-	points[3] = (t_fpoint){pos.x + hitbox.p1.x,
-		pos.y + hitbox.p2.y};
+	points[0] = (t_fpoint){pos.x + hitbox.p1.x + 1,
+		pos.y + hitbox.p1.y + 1};
+	points[1] = (t_fpoint){pos.x + hitbox.p2.x - 1,
+		pos.y + hitbox.p1.y + 1};
+	points[2] = (t_fpoint){pos.x + hitbox.p2.x - 1,
+		pos.y + hitbox.p2.y - 1};
+	points[3] = (t_fpoint){pos.x + hitbox.p1.x + 1,
+		pos.y + hitbox.p2.y - 1};
 	collision = 0;
 	i = -1;
 	while (++i < 4)
